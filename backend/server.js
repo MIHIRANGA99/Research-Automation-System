b9ef1873945b120bb0bb761641fb601fdc6b760a
MIHIRANGA99 <kavisha.mihiranga@yahoo.com>
2022-05-21 21:36:45 +0530

    database connected

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,15 @@
+import { connect } from "./utils/db.connection.js";
+import 'dotenv/config'
+import json from "koa-json";
 import Koa from "koa";
 
 const app = new Koa();
 
-app.listen(5000, () => {
-  console.log("Server Started!");
+const PORT = process.env.PORT || "8090";
+
+app.use(json());
+
+app.listen(PORT, () => {
+  console.log("Server Started on port : " + PORT);
+  connect();
 });
