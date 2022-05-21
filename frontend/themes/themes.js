396b084ba36fac46384840fd2c395e12299bedca
MIHIRANGA99 <kavisha.mihiranga@yahoo.com>
2022-05-21 17:05:57 +0530

    initial commit

---
+++ frontend/themes/themes.js
@@ -0,0 +1,27 @@
+import { createTheme } from "@mui/material/styles";
+
+export const dashboard = createTheme({
+  palette: {
+    primary: {
+      main: "#FFCE97",
+      dark: "#FCBD75",
+    },
+    secondary: {
+      main: "#FFCE97",
+      dark: "#FCBD75",
+    },
+  },
+});
+
+export const deleteButton = createTheme({
+  palette: {
+    primary: {
+      main: "#850000",
+      dark: "#670000",
+    },
+    secondary: {
+      main: "#850000",
+      dark: "#670000",
+    },
+  },
+});
