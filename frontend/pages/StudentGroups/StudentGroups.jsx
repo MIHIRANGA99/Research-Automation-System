53c01b1d9e28f53963acb9b2b4b3ec139b6d9dff
MIHIRANGA99 <kavisha.mihiranga@yahoo.com>
2022-06-04 12:47:14 +0530

    bugFix: allocate panel

--- frontend/pages/StudentGroups/StudentGroups.jsx
+++ frontend/pages/StudentGroups/StudentGroups.jsx
@@ -1,19 +1,8 @@
-import {
-  Button,
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogTitle,
-  ThemeProvider,
-} from "@mui/material";
 import React, { useEffect, useState } from "react";
-import AllocateButton from "../../components/Buttons/AllocateButton";
+import GroupCard from "../../components/Cards/GroupCard";
 import {
-  getGroups,
-  updateGroup,
+  getGroups
 } from "../../controllers/studentGroupController";
-import { getStaff } from "../../controllers/userController";
-import { dialog } from "../../themes/themes";
 import "./StudentGroups.css";
 
 const StudentGroups = () => {
@@ -33,120 +22,14 @@ const StudentGroups = () => {
       });
   }, []);
 
-  const handlePop = () => {
-    getStaff
-      .then((res) => setStaffData(res.data))
-      .catch((e) => console.log(e.message));
-    setPopup(true);
-  };
-
-  const insertToArray = (memberID) => {
-    if (allocatedArray.includes(memberID)) {
-      alert("Already Exists!");
-    } else {
-      setAllocatedArray([...allocatedArray, memberID]);
-    }
-  };
-
-  const allocatePanel = (id) => {
-
-    const info = {
-      panel: allocatedArray,
-      allocated: true
-    }
-
-    updateGroup(id, info)
-      .then((res) => {
-        console.log(res.data);
-      })
-      .catch((e) => {
-        console.log(e.message);
-      });
-  };
-
   return (
     <>
       <div className="GroupPage">
         <div className="groupsGrid">
           {groupData.map((group, index) => (
-            <div key={index} className="groupCard">
-              <label style={{ fontSize: "20px" }}>{group.groupName}</label>
-              <div className="layout">
-                <div className="listView">
-                  <div style={{ color: "#FFCE97" }}>Group</div>
-                  {group.students.map((student, idx) => (
-                    <label key={idx}>{student}</label>
-                  ))}
-                </div>
-                <div className="listView">
-                  <div style={{ color: "#FFCE97" }}>Panel</div>
-                  {group.panel?.map((student, idx) => (
-                    <label key={idx}>{student}</label>
-                  ))}
-                </div>
-              </div>
-              <AllocateButton
-                disabled={!!group.panel.length}
-                buttonText={
-                  !!group.panel.length > 0 ? (
-                    <label style={{ color: "#ffce97be" }}>
-                      Panel Allocated
-                    </label>
-                  ) : (
-                    "Allocate a Panel"
-                  )
-                }
-                variant="contained"
-                onClick={() => handlePop()}
-              />
-              {/*Allocate Popup*/}
-              <ThemeProvider theme={dialog}>
-                <Dialog
-                  className="addSTDialog"
-                  open={popup}
-                  onClose={() => setPopup(false)}
-                  aria-labelledby="alert-dialog-title"
-                  aria-describedby="alert-dialog-description"
-                >
-                  <DialogTitle className="dialogTitle" id="alert-dialog-title">
-                    {"Allocate Panel Members"}
-                  </DialogTitle>
-                  <DialogContent className="popUp">
-                    <div className="staffPopup">
-                      {staffData.map((member, index) => {
-                        return (
-                          <div
-                            className={
-                              allocatedArray.includes(member._id)
-                                ? "staffMemberCardActive"
-                                : "staffMemberCard"
-                            }
-                            onClick={() => insertToArray(member._id)}
-                            key={index}
-                          >
-                            {member.name}
-                            <label className="status">
-                              {allocatedArray.includes(member._id) &&
-                                "Selected"}
-                            </label>
-                          </div>
-                        );
-                      })}
-                    </div>
-                  </DialogContent>
-                  <DialogActions>
-                    <Button
-                      variant="contained"
-                      onClick={() => allocatePanel(group._id)}
-                      autoFocus
-                    >
-                      Allocate
-                    </Button>
-                  </DialogActions>
-                </Dialog>
-              </ThemeProvider>
-            </div>
-          ))}
+              <GroupCard grpID={group._id} grpName={group.groupName} panel={group.panel} students={group.students} key ={index} />
+          )
+          )}
         </div>
       </div>
     </>
